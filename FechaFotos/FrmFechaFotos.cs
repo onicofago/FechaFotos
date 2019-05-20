5af31ca167c4253397f553501ef9ebaed3800934
drobledobw <diego.robledo@tarmac.io>
2019-05-20 10:02:45 -0300

    Reordered usings

--- FechaFotos/FrmFechaFotos.cs
+++ FechaFotos/FrmFechaFotos.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using FechaFotos.Managers;
+using System;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
-using FechaFotos.Managers;
 
 namespace FechaFotos
 {
--- FechaFotos/Managers/FileManager.cs
+++ FechaFotos/Managers/FileManager.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FechaFotos.Managers
 {
@@ -70,3 +66,4 @@ namespace FechaFotos.Managers
         }
     }
 }
+
